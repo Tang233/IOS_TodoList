40309820d29d17cee2c7d6688f4b8b569bb01f7c
Tang233 <tangjinzoe@gmail.com>
2022-05-16 02:55:14 +1000

    Add devices adaptation

--- TodoList/TodoList/Views/AddView.swift
+++ TodoList/TodoList/Views/AddView.swift
@@ -22,7 +22,7 @@ struct AddView: View {
                 TextField("Type something here...", text: $textFieldText)
                     .padding(.horizontal)
                     .frame(height: 55)
-                    .background(Color(white: 0.9))
+                    .background(Color(UIColor.secondarySystemBackground))
                     .cornerRadius(10)
                Button(
                     action: saveButtonPressed,
